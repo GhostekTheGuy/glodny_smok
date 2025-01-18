fa5df0352b0fa0527dbcd4269f07e5d95565a1be
Hubert Kolejko <83242010+GhostekTheGuy@users.noreply.github.com>
2025-01-18 16:29:45 +0100

    first

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        qwigley: ['var(--font-qwigley)'],
+      },
+    },
+  },
+} 
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        poppins: ['var(--font-poppins)'],
+        qwigley: ['var(--font-qwigley)'],
+        pacifico: ['var(--font-pacifico)'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
